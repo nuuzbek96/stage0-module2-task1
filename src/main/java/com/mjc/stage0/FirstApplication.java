d0b6d345de28cdb9ce3df3f39ffe039928b25d80
Behruz <nuuzbek96@gmail.com>
2023-04-11 17:47:42 +0500

    finishedd

--- src/main/java/com/mjc/stage0/FirstApplication.java
+++ src/main/java/com/mjc/stage0/FirstApplication.java
@@ -7,6 +7,6 @@ import java.util.stream.Stream;
 
 public class FirstApplication {
     public static void main(String[] args) {
-        System.out.print("Hello, World!");
+        System.out.println("Hello, World!");
     }
 }
