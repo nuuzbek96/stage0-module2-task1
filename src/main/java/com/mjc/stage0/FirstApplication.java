49f7058e54c88d5e0fdd8f18407c84c7bb9616a6
Behruz <nuuzbek96@gmail.com>
2023-04-11 17:42:46 +0500

    finished

--- src/main/java/com/mjc/stage0/FirstApplication.java
+++ src/main/java/com/mjc/stage0/FirstApplication.java
@@ -1,7 +1,12 @@
 package com.mjc.stage0;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class FirstApplication {
     public static void main(String[] args) {
-        // Write your code here!
+        System.out.print("Hello, World!");
     }
 }
